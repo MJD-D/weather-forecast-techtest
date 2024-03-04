13f637f78aeea38b538fb82581cee4ae4a4a36c6
MJD-D <matthewdalby1995+techtest@gmail.com>
2024-03-04 17:46:03 +0000

    added tests for timeslots

---
+++ src/app/components/dayComponents/Timeslot.test.tsx
@@ -0,0 +1,24 @@
+import { Timeslot } from "./Timeslot"
+import {render, screen} from '@testing-library/react'
+
+const mockTimeslotData = {
+    dt : 1661871600,
+    icon : "10d",
+    description : "light rain",
+    humidity: 69,
+    temp :7.48,
+    unitSymbol: "°C"
+}
+
+describe('Timeslot', ()=> {
+    test('renders Timeslot correctly', async () => {
+        render(<Timeslot {...mockTimeslotData}/>);
+        screen.debug()
+        expect(screen.getByAltText("light rain")).toBeVisible();
+        expect(screen.getByText("Weather: light rain")).toBeVisible();
+        expect(screen.getByText("Temperature: 7.48 °C")).toBeVisible();
+        expect(screen.getByText("Humidity: 69%")).toBeVisible();
+        expect(screen.getByText("Time: 16:00")).toBeVisible();
+
+    })
+})
